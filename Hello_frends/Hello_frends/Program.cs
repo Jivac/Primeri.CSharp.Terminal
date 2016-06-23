ef20c0a1d6381c135f7e3396c3b235467eb29d4a
Jivac <dim_jiv@yahoo.com>
2016-06-23 09:57:08 +0300

    Създаване на програма Hello_frends

---
+++ Hello_frends/Hello_frends/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_frends
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello_frends");
+			Console.ReadKey ();
+		}
+	}
+}
