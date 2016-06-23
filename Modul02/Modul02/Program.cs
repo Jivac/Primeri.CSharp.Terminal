7c28855f5592b52bbe8b028f5c3180fee8899939
Jivac <dim_jiv@yahoo.com>
2016-06-23 10:18:40 +0300

    Модул 02, Видео01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			//Дефиниране на променливи
+
+			//събиране на числа
+
+			//Писане в конзола
+		}
+	}
+}
