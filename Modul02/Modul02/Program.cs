fecc80874ca5046cae06b4b21faec56f6fefb760
Jivac <dim_jiv@yahoo.com>
2016-06-24 10:25:37 +0300

    Модул 02, Видео 04

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -9,12 +9,18 @@ namespace Modul02
 		//Дефиниране на променливи
 			int a = 0, b = 0;
 			int sum = 0;
+			string program_name = "Modul02 - Примери";
+			string program_version = "1.0";
 
 			//събиране на числа
 			a = 25;
 			b = 7;	
 
 			sum = a + b;
+
+			//За програмата
+			Console.WriteLine (program_name );
+			Console.WriteLine ("Версия" + program_version + "\n\n");
 
 			//Писане в конзола
 			Console.Write ( a );
@@ -24,24 +30,25 @@ namespace Modul02
 			Console.WriteLine ( sum );
 
 			//Допълнителни оператори
-			Console.WriteLine ("\nИзползване на +=");
-			a += b; // или a = a + b
-			Console.WriteLine ( a );
+						Console.WriteLine ("\nИзползване на +=");
+						a += b; // или a = a + b
+						Console.WriteLine ( a );
 
-			Console.WriteLine ("\nИзползване на -=");
-			a -= b; // или a = a - b
-			Console.WriteLine ( a );
+						Console.WriteLine ("\nИзползване на -=");
+						a -= b; // или a = a - b
+						Console.WriteLine ( a );
 
-			Console.WriteLine ("\nИзползване на *=");
-			a *= b; // или a = a * b
-			Console.WriteLine ( a );
+						Console.WriteLine ("\nИзползване на *=");
+						a *= b; // или a = a * b
+						Console.WriteLine ( a );
 
-			Console.WriteLine ("\nИзползване на ++");
-			a++; // или a = a + 1
-			Console.WriteLine ( a );
+						Console.WriteLine ("\nИзползване на ++");
+						a++; // или a = a + 1
+						Console.WriteLine ( a );
 
-			Console.WriteLine ("Стандартно делене" + ( 14 / 4).ToString());
-			Console.WriteLine ("Остатък от делене" + (14 %4).ToString ());
+						Console.WriteLine ("Стандартно делене" + ( 14 / 4).ToString());
+						Console.WriteLine ("Остатък от делене" + (14 %4).ToString ());
+
 		}
 	}
 } 
